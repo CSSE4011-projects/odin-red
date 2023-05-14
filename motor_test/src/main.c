1ff21842ab67f9c5275706f01365af58b2c4be06
Lachlan <l.edwards2@uqconnect.edu.au>
2023-05-14 19:53:23 +1000

    code style improvements

--- motor_test/src/main.c
+++ motor_test/src/main.c
@@ -1,19 +1,21 @@
 
 #include <stdlib.h>
-#include <zephyr/kernel.h>
 
+#include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 #include <zephyr/device.h>
 #include <zephyr/shell/shell.h>
-
 #include <zephyr/drivers/i2c.h>
 
 #include "rovermotor.h"
 #define LOG_LEVEL 4
 LOG_MODULE_REGISTER(main);
+
+//  Global to pass to shell commands. 
 struct rovermotor_info motor_control_handle; 
 
 const struct device *i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
+
 static int rover_cmd_cb(const struct shell* shell, size_t argc, char** argv) 
 {
 	int8_t vel = atoi(argv[1]); 
@@ -27,7 +29,6 @@ static int rover_cmd_cb(const struct shell* shell, size_t argc, char** argv)
 void main(void) 
 {
 
-	
 	rovermotor_init(0x5D, 0xAB, i2c_dev, &motor_control_handle); 
 	LOG_INF("Initialized motor handler");
 
@@ -35,33 +36,17 @@ void main(void)
         rover,
         NULL,
         "rover [speed] [angle]: set speed and angle \n"
-		"speed from -127 to 127, velocity from -100 to 100 (int)",
+		"speed from -127 to 127, angle from -100 to 100 (int)",
         rover_cmd_cb,
         3,
         0
     );
 
-	int8_t fwd = 0;
 	while (1)
 	{
-		/*
-		for (int i=0; i<255; i++) {
-			motordriver_send_pwm(i2c_dev, addr, i, 0);
-			k_sleep(K_MSEC(100));
-			LOG_INF("Sending: %hhu", i);
-		}
-		*/
+		// Do nothing - shell command handles it all. 
 		k_sleep(K_MSEC(100));
-		//LOG_INF("Sending instruction");
-
-		/*
-		for (float i=-1; i<1; i = i + 0.1) 
-		{
-			rovermotor_send_instruction(&motor_control_handle, i, fwd);
-			k_sleep(K_MSEC(100));
-		}
-		*/
-		//rovermotor_send_instruction(&motor_control_handle, 0, 60);
+		
 	}
 	
 	
--- motor_test/src/motordriver.c
+++ motor_test/src/motordriver.c
@@ -1,6 +1,10 @@
 #include "motordriver.h"
 LOG_MODULE_REGISTER(motordriver);
 
+/**
+ * initializes an instance of a driver for the Sparkfun Qwiic Motor Driver /
+ * Sparkfun SCMD motor driver, on given uart device, with given device address
+ */
 void motordriver_init(const struct device* dev, uint8_t device_addr) 
 {
 
@@ -8,17 +12,17 @@ void motordriver_init(const struct device* dev, uint8_t device_addr)
 		LOG_ERR("I2C: Device is not ready.\n");
 		return;
 	}
-    // dummy read: 
+    // dummy read - check the device we expect is on this bus. 
     uint8_t res;
     int status = i2c_reg_read_byte(dev, device_addr, SCMD_ID, &res);
     int num_tries = 0; 
-    while (res != 0xA9) 
+    while (res != ID_WORD) 
     {
         LOG_INF("Couldn't get valid ID yet ... ");
 
         k_sleep(K_MSEC(100));
         status = i2c_reg_read_byte(dev, device_addr, SCMD_ID, &res);
-        if (num_tries++ > 10) {
+        if (num_tries++ > CONN_MAX_NUM_TRIES) {
             LOG_ERR("Couldn't find driver with addr %x", device_addr);
             return; 
         };
@@ -33,42 +37,10 @@ void motordriver_init(const struct device* dev, uint8_t device_addr)
 
     i2c_reg_read_byte(dev, device_addr, SCMD_DRIVER_ENABLE, &res);
     i2c_reg_write_byte(dev, device_addr, SCMD_BRIDGE, 0x01);
-    return; 
 }
 
 int motordriver_send_pwm(const struct device* dev, uint8_t device_addr, uint8_t val)
 {
-
-	//struct i2c_msg msgs[2];
-
-	/* Send the address to write to */
-    /*
-    uint8_t msg_address = SCMD_MA_DRIVE + is_left;
-	msgs[0].buf = &msg_address;
-	msgs[0].len = 1;
-	msgs[0].flags = I2C_MSG_WRITE;
-    */ 
-	/* Data to be written, and STOP after this. */
-    /*
-	msgs[1].buf = &val;
-	msgs[1].len = 1;
-	msgs[1].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
-
-	return i2c_transfer(dev, &msgs[0], 2, device_addr);
-
-    */
-    // Might also work:
-    
-    //uint8_t buf[] = {SCMD_MA_DRIVE + is_left, val};
-
-    uint8_t buf = val;
-    //uint16_t data_addr = device_addr << 8 || (SCMD_MA_DRIVE + is_left);  
-    uint16_t data_addr = 0x5D20;
-    //int res = i2c_write(dev, &buf, sizeof(buf), data_addr);
-    //uint8_t rd_buf[2]; 
-    //int read_res = i2c_read(dev, &rd_buf[0], 2, device_addr); 
-
-
     int res = i2c_reg_write_byte(dev, device_addr, SCMD_MA_DRIVE, val);
     if (res) {
         return res;
